8e37e420ae68cf6e16917d6e3242576443f615b2
Yerson Rodriguez <rodriguezyerson2005@gmail.com>
2025-04-29 18:56:15 -0500

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2');
 
 // Crear la conexión
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'citas_medicas'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection;
