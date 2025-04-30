1198ba00b1de9c08a6ae4786bebfa47e71b21dcf
Yerson Rodriguez <rodriguezyerson2005@gmail.com>
2025-04-29 20:27:30 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout: 10000
 });
 
 module.exports = connection;
